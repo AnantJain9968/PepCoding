3c5633c621a93f2e35b32c7484ce3b0522a03de6
AnantJain9968 <anantjain9968@gmail.com>
2021-06-25 17:11:30 +0530

    Added multiple cell selection

--- ExcelClone/script.js
+++ ExcelClone/script.js
@@ -13,6 +13,7 @@ for(let i=1;i<=100;i++){
             n= Math.floor(n/26);
         }
     }
+    console.log(ans);
     console.log(i);
    let columnName= $(`<div class="column-name  colId-${i}" id="colCod-${ans}">${ans}</div>`);
    $(".column-name-container").append(columnName);
@@ -20,10 +21,10 @@ for(let i=1;i<=100;i++){
    $(".row-name-container").append(rowName);
 }
 for(let i=1;i<=100;i++){
-    let row=$(`<div class="cell-row">`);
+    let row=$(`<div class="cell-row"></div>`);
     for(j=1;j<=100;j++){
        let colCode = $(`.colId-${j}`).attr('id').split("-")[1];
-       let column = $(`<div class="input-cell" contenteditable="true" id="row-${i}-col-${j}" data=${colCode}></div>`);
+       let column = $(`<div class="input-cell" contenteditable="false" id="row-${i}-col-${j}" data=${colCode}></div>`);
        row.append(column);
     }
     $(".input-cell-container").append(row);
@@ -36,10 +37,99 @@ $('.align-icon').click(function() {
 $('.style-icon').click(function(){
     $(this).toggleClass("selected");
 })
-$(".input-cell").click(function(){
-    $('.input-cell.selected').removeClass("selected");
-    $(this).addClass("selected")
+$(".input-cell").click(function(e){
+    console.log(e);
+     if(e.ctrlKey){
+        let [rowId,colId]= getRowCol(this);
+        $(this).addClass("selected")
+        if(rowId>1){
+            let istopCellSelected=$(`#row-${rowId-1}-col-${colId}`).hasClass("selected");
+            if(istopCellSelected){
+                $(`#row-${rowId-1}-col-${colId}`).addClass("bottom-cell-selected");
+                $(`#row-${rowId}-col-${colId}`).addClass("top-cell-selected");
+            }
+        }
+        if(rowId<100){
+            let isbottomCellSelected=$(`#row-${rowId+1}-col-${colId}`).hasClass("selected");
+            if(isbottomCellSelected){
+                $(`#row-${rowId+1}-col-${colId}`).addClass("top-cell-selected");
+                $(`#row-${rowId}-col-${colId}`).addClass("bottom-cell-selected");
+            }
+        }
+        if(colId>1){
+            let isleftCellSelected=$(`#row-${rowId}-col-${colId-1}`).hasClass("selected");
+            if(isleftCellSelected){
+                $(`#row-${rowId}-col-${colId-1}`).addClass("right-cell-selected");
+                $(`#row-${rowId}-col-${colId}`).addClass("left-cell-selected");
+            }
+        }
+        if(colId<100){
+            let isrightCellSelected=$(`#row-${rowId}-col-${colId+1}`).hasClass("selected");
+            if(isrightCellSelected){
+                $(`#row-${rowId}-col-${colId+1}`).addClass("left-cell-selected");
+                $(`#row-${rowId}-col-${colId}`).addClass("right-cell-selected");
+            }
+        }
+        // $(this).addClass("selected")
+     }
+     else{
+        $('.input-cell.selected').removeClass("selected");
+        $(this).addClass("selected")
+     }
+    
 })
+$(".input-cell").dblclick(function(){
+    $('.input-cell.selected').removeClass("selected");
+    $(this).addClass("selected");
+    $(this).attr('contenteditable','true');
+    $(this).focus();
+})
+$(".input-cell").blur(function(){
+    $(this).attr('contenteditable','false');
+})
+
+$(".input-cell-container").scroll(function () {
+    $(".column-name-container").scrollLeft(this.scrollLeft);
+    $(".row-name-container").scrollTop(this.scrollTop);
+})
+
+function getRowCol(e){
+    let idArray = $(e).attr('id').split('-');
+    let rowId=parseInt(idArray[1]);
+    let colId=parseInt(idArray[3]);
+    return [rowId,colId];
+}
+
+function updateCell(property,value){
+    $('.input-cell.selected').each(function(){
+        $(this).css(property,value);
+    })
+}
+
+
+$(".icon-bold").click(function() {
+    if($(this).hasClass("selected")) {
+        updateCell("font-weight","bold");
+    } else {
+        updateCell("font-weight","");
+    }
+});
+
+$(".icon-italic").click(function() {
+    if($(this).hasClass("selected")) {
+        updateCell("font-style","italic");
+    } else {
+        updateCell("font-style","");
+    }
+});
+
+$(".icon-underline").click(function() {
+    if($(this).hasClass("selected")) {
+        updateCell("text-decoration","underline");
+    } else {
+        updateCell("text-decoration","");
+    }
+});
 
 });
 
